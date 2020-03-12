f97acdf76d24347dde3168b7d0a0c21cf477cc94
peteplass <pplass@gmail.com>
2020-03-12 11:04:54 -0700

    creating slides

--- fire_flask.py
+++ fire_flask.py
@@ -16,21 +16,18 @@ def form():
 
 #submission page
 @app.route('/submit')
-# load in the form data from the incoming request
+
 def submit():
     user_input = request.args
 
     date = user_input['date']
     date_present = date[5:]
-    # date = jsonify({'date' : date})
     ref=pd.read_json('../Cal_Fire/preds.json', typ='dictionary')
-    # model = pickle.load(open('assets/model.p', 'rb'))
-    # preds = model.predict(X_test)
+
 
     pred = ref[date].round()
     return render_template('results.html', pred=pred, date=date_present)
-# manipulate data into a format that we pass to our model
-# Call app.run(debug=True) when python script is called
 
-if __name__ == '__main__': # if we run the file (app_starter.py) from the terminal
+
+if __name__ == '__main__': # if we run the file (fire_flask.py) from the terminal
     app.run(debug=True)
